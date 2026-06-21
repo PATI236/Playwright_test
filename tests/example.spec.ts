90b7ae3a2aaaf17c0ae7659b7243eaecf4b0c475
Subham Pati <mahbus56@gmail.com>
2026-06-21 18:59:54 +0530

    added sharts and env

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,37 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('/');
+});
+test('has title1', async ({ page }) => {
+  await page.goto('/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+});
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('get started link1', async ({ page }) => {
+  await page.goto('/');
+});
+test('has title2', async ({ page }) => {
+  await page.goto('/');
+
+});
+
+test('get started link2', async ({ page }) => {
+  await page.goto('/');
+});
+
+test('has title3', async ({ page }) => {
+  await page.goto('/');
+
+});
+
+test('get started link3', async ({ page }) => {
+  await page.goto('/');
 });
 
 
